1c3b35f2f007590e804ef5df48d6fd2f9efefbeb
unknown <john.gordos@outlook.com>
2023-07-03 10:45:09 -0600

    updated home route to query by fruit of the day

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,28 +23,18 @@ router.get("/", async (req, res) => {
     const sightings = sightingData.map((sighting) =>
       sighting.get({ plain: true })
     );
-    var fruitOfTheDay;
-    if (
-      !req.session.dateSelected ||
-      (req.session.dateSelected &&
-        req.session.dateSelected + 60 * 60 * 1000 * 24 <= Date.now())
-    ) {
-      const fruitData = await Fruit.findAll({
-        order: [Sequelize.fn("RAND")],
-        limit: 1,
-      });
-      fruitOfTheDay = fruitData[0].get({ plain: true });
-      req.session.fruit = fruitOfTheDay;
-      req.session.dateSelected = Date.now();
-    } else {
-      fruitOfTheDay = req.session.fruit;
-    }
+    const fruitOfTheDay = await Fruit.findAll({
+      limit: 1,
+      where: {
+        fruit_of_the_day: true,
+      },
+    });
     res.json({
       fruitOfTheDay,
       sightings,
       loggedIn: req.session.loggedIn,
     }); //REMOVE AFTER TESTING
-    //res.render("homepage", { sightings, fruitOfTheDay }); //homepage to display message if there is no sightning data
+    //res.render("homepage", { sightings, fruitOfTheDay[0].get("{plain: true}"), loggedIn: req.session.loggedIn }); //homepage to display message if there is no sightning data
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
