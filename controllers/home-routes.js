1aeb58a13355b811720668247bdd87ce4b1c3229
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-07-05 12:14:45 -0400

    Fixed MIME error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,38 +4,38 @@ const { Sighting, Fruit, Location } = require("../models");
 
 //Get the 10 most recent sightings and a random fruit to display on the homepage.
 router.get("/", async (req, res) => {
-  // try {
-  //   const sightingData = await Sighting.findAll({
-  //     limit: 10,
-  //     order: [["created_At", "DESC"]],
-  //     include: [
-  //       {
-  //         model: Fruit,
-  //         attributes: ["name"],
-  //       },
-  //       {
-  //         model: Location,
-  //         attributes: ["name", "city", "state"],
-  //       },
-  //     ],
-  //   });
+  try {
+    const sightingData = await Sighting.findAll({
+      limit: 10,
+      order: [["created_At", "DESC"]],
+      include: [
+        {
+          model: Fruit,
+          attributes: ["name"],
+        },
+        {
+          model: Location,
+          attributes: ["name", "city", "state"],
+        },
+      ],
+    });
 
-  //   const sightings = sightingData.map((sighting) =>
-  //     sighting.get({ plain: true })
-  //   );
+    const sightings = sightingData.map((sighting) =>
+      sighting.get({ plain: true })
+    );
 
-  //   const fruitData = await Fruit.findAll({
-  //     order: [Sequelize.fn("RAND")],
-  //     limit: 1,
-  //   });
-  //   res.json({ fruitData, sightings, loggedIn: req.session.loggedIn }); //REMOVE AFTER TESTING
-  //   const randomFruit = fruitData[0].get({ plain: true });
-  // res.render("homepage", { sightings, randomFruit });
-  res.render("homepage"); //homepage to display message if there is no sightning data
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   }
+    const fruitData = await Fruit.findAll({
+      order: [Sequelize.fn("RAND")],
+      limit: 1,
+    });
+    res.json({ fruitData, sightings, loggedIn: req.session.loggedIn }); //REMOVE AFTER TESTING
+    const randomFruit = fruitData[0].get({ plain: true });
+    res.render("homepage", { sightings, randomFruit });
+    //homepage to display message if there is no sightning data
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", (req, res) => {
